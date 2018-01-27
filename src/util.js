3df63caf42ac2598bc1040ae2ec91c041c20ff5a
hai <whzzyz@21cn.com>
2018-01-27 14:32:45 +0800

    browser.js

--- src/util.js
+++ src/util.js
@@ -1,21 +1,30 @@
-export var __push = Array.prototype.push;
+
+
 export var hasSymbol = typeof Symbol === "function" && Symbol["for"]; 
+
 export var REACT_ELEMENT_TYPE = hasSymbol ? Symbol["for"]("react.element") : 0xeac7;
-export var innerHTML = "dangerouslySetInnerHTML";
-export var hasOwnProperty = Object.prototype.hasOwnProperty;
 export var REACT_FRAGMENT_TYPE = hasSymbol ? Symbol["for"]("react.fragment") : 0xeacb;
+
+export var innerHTML = "dangerouslySetInnerHTML";
+
+export var __push = Array.prototype.push;
+export var hasOwnProperty = Object.prototype.hasOwnProperty;
 
 export var emptyArray = [];
 export var emptyObject = {};
+
+//过时的警告
 export function deprecatedWarn(methodName) {
+    //判断是否存放过改警告
     if (!deprecatedWarn[methodName]) {
         //eslint-disable-next-line
         console.warn(methodName + " is deprecated");
         deprecatedWarn[methodName] = 1;
     }
 }
+
 /**
- * 复制一个对象的属性到另一个对象
+ * 复制一个对象的属性到另一个对象  ！复制第一层！
  *
  * @param {any} obj
  * @param {any} props
@@ -29,6 +38,7 @@ export function extend(obj, props) {
     }
     return obj;
 }
+
 export function returnFalse() {
     return false;
 }
@@ -54,21 +64,30 @@ export function noop() {}
  */
 export function inherit(SubClass, SupClass) {
     function Bridge() {}
+
     let orig = SubClass.prototype;
     Bridge.prototype = SupClass.prototype;
+
+    // SubClass.prototype = { __proto__: Sublcass.prototype  , ... Subclass构造函数的各种this属性键值对}
     let fn = (SubClass.prototype = new Bridge());
 
-    // 避免原型链拉长导致方法查找的性能开销
+    // 避免原型链拉长导致方法查找的性能开销, 
+    //由于是地址引用， 所以fn扩展了orig的属性， 相当于 SubClas.prototype也重新追加自己的原型属性，
+    //这里是继承， 但是自己的东西还是不会少的 ， \(^o^)/~  
+
     extend(fn, orig);
+
     fn.constructor = SubClass;
     return fn;
 }
 
 var lowerCache = {};
+
 export function toLowerCase(s) {
     return lowerCache[s] || (lowerCache[s] = s.toLowerCase());
 }
 
+//实实在在清空数组
 export function clearArray(a) {
     return a.splice(0, a.length);
 }
@@ -77,8 +96,15 @@ export function isFn(obj) {
     return __type.call(obj) === "[object Function]";
 }
 
+//判断一个字符串， 非 逗号 空格 的所有字符， 如：
+
+//'abc ddd, adaf'.match(rword)
+//(3) ["abc", "ddd", "adaf"]
+
 var rword = /[^, ]+/g;
 
+//改造字符串 或者 数组 成一个对象形式 
+//{ a: 1, b:1, c: 1}
 export function oneObject(array, val) {
     if (array + "" === array) {
         //利用字符串的特征进行优化，字符串加上一个空字符串等于自身
@@ -93,24 +119,35 @@ export function oneObject(array, val) {
     return result;
 }
 
+//匹配 abc-abc  abc_abc 这样的字符串
 var rcamelize = /[-_][^-_]/g;
+
 export function camelize(target) {
     //提前判断，提高getStyle等的效率
     if (!target || (target.indexOf("-") < 0 && target.indexOf("_") < 0)) {
         return target;
     }
-    //转换为驼峰风格
+
+
+    //转换为驼峰风格  abc-abc = >  abcAbc
     var str = target.replace(rcamelize, function(match) {
+        //match就是-a; 
+        //把-a 替换成 A
         return match.charAt(1).toUpperCase();
     });
+
+
     return firstLetterLower(str);
 }
 
+//把第一个字符变成小写 AbcAbc => abcAbc
 export function firstLetterLower(str) {
     return str.charAt(0).toLowerCase() + str.slice(1);
 }
 
-export var options = oneObject(["beforeProps", "afterCreate", "beforeInsert", "beforeDelete", "beforeUpdate", "afterUpdate", "beforePatch", "afterPatch", "beforeUnmount", "afterMount"], noop);
+//为这些React生命周期函数统一赋值
+export var options = oneObject(["beforeProps", "afterCreate", "beforeInsert", "beforeDelete",
+ "beforeUpdate", "afterUpdate", "beforePatch", "afterPatch", "beforeUnmount", "afterMount"], noop);
 
 var numberMap = {
     //null undefined IE6-8这里会返回[object Object]
@@ -135,6 +172,7 @@ export function typeNumber(data) {
     return a || 8;
 }
 
+//转换类数组
 export var toArray =
     Array.from ||
     function(a) {
